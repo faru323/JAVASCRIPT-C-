c551927701ec5332bb5918ef6e8b7c73e992e08a
Fatima <bossfatya@gmail.com>
2022-11-03 13:57:44 +0400

    added

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = { 1, 2, 3, 4 };
+
+            for (int i = 0; i <  number.Length; ++i)
+            {
+               
+                Console.WriteLine(number[i]); 
+              
+            }
+        }
+    }
+}
---
+++ work.js
@@ -0,0 +1,19 @@
+function sum(a,b) {
+   // console.log(a+b);
+    return a+b;
+  }
+  function multiply(a,b) {
+   // console.log(a*b);
+    return a*b;
+  }
+  function subtract(a,b) {
+    a>b
+  //console.log(a-b);
+    return a-b;
+  }
+  function divide(a,b) {
+   // console.log(a/b);
+    return a/b;
+   
+  }
+ 
